de0467223f634a1a8e5114bfc81bfa7e216e8c4b
fabaidoo <>
2020-11-20 19:22:10 -0600

    Modified angle function

--- angle.m
+++ angle.m
@@ -1,6 +1,6 @@
-function [Ox, Oy, w] = angle(n_xy, n_z)
-% xy - direction discretization along with weights 
-
+function [phi, Oz, w_xy, w_z] = angle(n_xy, n_z)
+% produces angles phi in the x-y plane, directions in the z-axis Oz and 
+%weights for both phi and Oz 
 if mod(n_xy, 4) ~= 0
     error('Quarter symmetry is required for the problem')
 end
@@ -9,25 +9,18 @@ if mod(n_z, 2) ~= 0
 end
 
 w_xy = 2 * pi / n_xy;
-w_xy = w_xy *ones(1, n_xy);
-
 
 phi = pi * linspace(1/n_xy , (2*n_xy - 1)/n_xy, n_xy);%azimuthal angle 
 % SAME AS: 1/n: 2/n: (2*n - 1)/n
 
-[cos_theta, w_z] = angle_z(n_z); 
-sin_theta = sqrt(1 - cos_theta.^2 ); %polar component
-
-Ox =  kron(cos(phi), sin_theta); %x-directions 
-Oy = kron(sin(phi), sin_theta);
-
-w = kron(w_xy, w_z);
-
+[Oz, w_z] = angle_z(n_z); 
 
 function [mu, w_z] = angle_z(m)
 %Angular discretization in the z-direction
 if mod(m,2) == 1
-    error('Even angular discretization needed')   
+    error('Even angular discretization needed')  
+elseif m > 8
+    error('Highest available discretization is S8')
 end
 S2 = .5773502691896257;
 w2 = 1;
@@ -43,7 +36,6 @@ w8 = [.3626838 .3137066 .2223810 .1012285];
        
 Sn = {S2, S4, S6, S8};
 wn = {w2, w4, w6, w8};
-
 
 mu = Sn{m/2};
 w_z = wn{m/2};
